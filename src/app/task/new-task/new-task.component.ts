336104a5c418fd2a27d1ad4f2c1e27d965d2d986
Georgio01 <G.hassrouty@bsynchro.com>
2024-09-05 12:38:06 +0300

    change

--- src/app/task/new-task/new-task.component.ts
+++ src/app/task/new-task/new-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-task',
@@ -9,9 +9,4 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class NewTaskComponent {
 
-  @Output() cancel=new EventEmitter<void>
-
-  onCancel(){
-    this.cancel.emit();
-  }
 }
